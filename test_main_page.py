6e32f0b4543db3baced5880268dfbe8b91840ed5
n.dukov <n.dukov@keepsolid.com>
2020-09-01 10:00:14 +0300

    Stepik -> login page class added

--- test_main_page.py
+++ test_main_page.py
@@ -1,26 +1,27 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.basket_page import BasketPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.open_basket()
-    basket_page = BasketPage(browser, link)
-    basket_page.verify_no_product()
-    basket_page.verify_empty_basket_text()
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.open_basket()
+        basket_page = BasketPage(browser, link)
+        basket_page.verify_no_product()
+        basket_page.verify_empty_basket_text()
